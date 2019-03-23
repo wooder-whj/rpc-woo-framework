3dac68d3ccbea04e6b1cd44ce4aa3fc7db360d65
unknown <1153657192@qq.com>
2019-03-23 18:19:56 +0800

    modified:   woo-client/client/src/main/java/rpc/woo/framework/client/RpcClient.java

--- woo-client/client/src/main/java/rpc/woo/framework/client/RpcClient.java
+++ woo-client/client/src/main/java/rpc/woo/framework/client/RpcClient.java
@@ -1,9 +1,7 @@
 package rpc.woo.framework.client;
 
-import rpc.woo.framework.common.ProtocolBean;
-
 public interface RpcClient<P> {
      void send(P param);
-     ProtocolBean receive() throws InterruptedException;
+     P receive() throws InterruptedException;
      void close();
 }
